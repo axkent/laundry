bda0c22f206b534229b307e3e2b4d462d76a3ffc
Axel <axel.kent1@gmail.com>
2020-06-21 13:00:19 -0700

    fixed dryer cost

--- laundrycost.py
+++ laundrycost.py
@@ -2,11 +2,10 @@
 """laundrycost.py: Determine the cost of a laundry session """
 __author__      = "Axel K"
 
-#Laundry Quarter Calculator
 #washer: $1.50 per load
 #dryer: $1.00 for 45 min (minimum time to start dryer) and $.25 for each additional 15 min
 loadnumber = input('How many laundry loads?: ')
-normaldry = 0.45
+normaldry = 1
 hourdryer= input("How many 1 hour drying sessions?: ")
 laundrycost = (int(loadnumber) * 1.50) + int(normaldry) + (0.25*int(hourdryer))
 quartersreq = int(laundrycost/0.25)
